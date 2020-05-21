c2e23780096381337e1fa328cf8a3d69eaca5277
Shengwen Cheng <shengwen1997.tw@gmail.com>
2020-05-21 12:05:44 +0800

    rearrange code indent

--- dynamics.m
+++ dynamics.m
@@ -1,76 +1,76 @@
 classdef dynamics
-	properties
-	dt;      %[sec]
-	mass;    %[kg]
-
-	g = 9.8; %gravitational acceleration
-	
-	J;       %inertia matrix [kg*m^2]
-
-	x;       %initial position
-	v;       %initial velocity
-	a;       %initial acceleration
-	W;       %intial angular velocity 
-	W_dot;   %initial angular acceleration
-
-	R;       %attitude, direction cosine matrix
-	R_det;
-
-	prv_angle;
-
-	f;       %control force
-	M;       %control moment
-	end
-
-	methods
-	function f_next = integrator(obj, f_now, f_dot, dt)
-		%euler method
-		f_next = [f_now(1) + (f_dot(1) * dt);
-			  f_now(2) + (f_dot(2) * dt);
-			  f_now(3) + (f_dot(3) * dt)];
-	end
-
-	function ret_obj = update(obj)
-		math = se3_math;
-
-		%calculate angular velocity by integrating angular acceleration
-		obj.W = obj.integrator(obj.W, obj.W_dot, obj.dt);
-
-		%calculate velocity by integrating acceleration
-		obj.v = obj.integrator(obj.v, obj.a, obj.dt);
-
-		%calculate rotation matrix by intergrating DCM differential equation
-		%read "Direction Cosine Matrix IMU: Theory" for detailed explanation
-		Wdt = [obj.W(1) * obj.dt; obj.W(2) * obj.dt; obj.W(3) * obj.dt];
-		I = eye(3);
-		dR = math.hat_map_3x3(Wdt) + I;
-		%according to definition of SO(3), to rotate R by dR, the correct way
-		%to do is multiplication rather than addition, since SO(3) is not closed
-		%under addition
-		obj.R = obj.R * dR;
-		%maintain the orthonomal property of DCM
-		obj.R = math.dcm_orthonormalize(obj.R);
-		%to check the orthonormal propertiy of DCM, we can check the determinant of it
-		%obj.R_det = det(obj.R);
-		%disp(obj.R_det)
-
-		%calculate the angle of principle rotation vector
-		obj.prv_angle = math.get_prv_angle(obj.R);
-
-		%calculate position by integrating velocity
-		obj.x = obj.integrator(obj.x, obj.v, obj.dt);
-
-		%calculate current accelration from force
-		e3 = [0; 0; 1];
-		mv_dot = (obj.mass * obj.g * e3) - obj.f;
-		obj.a = mv_dot / obj.mass;
-
-		%calculate current aungular acceleration from moment
-		JW = obj.J * obj.W;
-		WJW = cross(obj.W, JW);
-		obj.W_dot = inv(obj.J) * (obj.M - WJW);
-
-		ret_obj = obj;
-	end
-	end
+    properties
+        dt;      %[sec]
+        mass;    %[kg]
+        
+        g = 9.8; %gravitational acceleration
+        
+        J;       %inertia matrix [kg*m^2]
+        
+        x;       %initial position
+        v;       %initial velocity
+        a;       %initial acceleration
+        W;       %intial angular velocity
+        W_dot;   %initial angular acceleration
+        
+        R;       %attitude, direction cosine matrix
+        R_det;
+        
+        prv_angle;
+        
+        f;       %control force
+        M;       %control moment
+    end
+    
+    methods
+        function f_next = integrator(obj, f_now, f_dot, dt)
+            %euler method
+            f_next = [f_now(1) + (f_dot(1) * dt);
+                f_now(2) + (f_dot(2) * dt);
+                f_now(3) + (f_dot(3) * dt)];
+        end
+        
+        function ret_obj = update(obj)
+            math = se3_math;
+            
+            %calculate angular velocity by integrating angular acceleration
+            obj.W = obj.integrator(obj.W, obj.W_dot, obj.dt);
+            
+            %calculate velocity by integrating acceleration
+            obj.v = obj.integrator(obj.v, obj.a, obj.dt);
+            
+            %calculate rotation matrix by intergrating DCM differential equation
+            %read "Direction Cosine Matrix IMU: Theory" for detailed explanation
+            Wdt = [obj.W(1) * obj.dt; obj.W(2) * obj.dt; obj.W(3) * obj.dt];
+            I = eye(3);
+            dR = math.hat_map_3x3(Wdt) + I;
+            %according to definition of SO(3), to rotate R by dR, the correct way
+            %to do is multiplication rather than addition, since SO(3) is not closed
+            %under addition
+            obj.R = obj.R * dR;
+            %maintain the orthonomal property of DCM
+            obj.R = math.dcm_orthonormalize(obj.R);
+            %to check the orthonormal propertiy of DCM, we can check the determinant of it
+            %obj.R_det = det(obj.R);
+            %disp(obj.R_det)
+            
+            %calculate the angle of principle rotation vector
+            obj.prv_angle = math.get_prv_angle(obj.R);
+            
+            %calculate position by integrating velocity
+            obj.x = obj.integrator(obj.x, obj.v, obj.dt);
+            
+            %calculate current accelration from force
+            e3 = [0; 0; 1];
+            mv_dot = (obj.mass * obj.g * e3) - obj.f;
+            obj.a = mv_dot / obj.mass;
+            
+            %calculate current aungular acceleration from moment
+            JW = obj.J * obj.W;
+            WJW = cross(obj.W, JW);
+            obj.W_dot = inv(obj.J) * (obj.M - WJW);
+            
+            ret_obj = obj;
+        end
+    end
 end
--- se3_math.m
+++ se3_math.m
@@ -1,98 +1,98 @@
 classdef se3_math
-	methods
-	function dcm = euler_to_dcm(obj, roll, pitch ,yaw)
-		%R = Rz(psi)Ry(theta)Rx(phi)
-		%read: https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
-		cos_phi = cos(double(roll));
-		cos_theta = cos(pitch);
-		cos_psi = cos(yaw);
-		sin_phi = sin(roll);
-		sin_theta = sin(pitch);
-		sin_psi = sin(yaw);
-		dcm11 = cos_theta * cos_psi;
-		dcm12 = (-cos_phi * sin_psi) + (sin_phi * sin_theta * cos_psi);
-		dcm13 = (sin_phi * sin_psi) + (cos_phi * sin_theta * cos_psi);
-		dcm21 = cos_theta * sin_psi; 
-		dcm22 = (cos_phi * cos_psi) + (sin_phi * sin_theta * sin_psi);
-		dcm23 = (-sin_phi * cos_psi) + (cos_phi * sin_theta * sin_psi);
-		dcm31 = -sin_theta;
-		dcm32 = sin_phi * cos_theta;
-		dcm33 = cos_phi * cos_theta;
-		dcm = [dcm11 dcm12 dcm13;
-		       dcm21 dcm22 dcm23;
-		       dcm31 dcm32 dcm33;];
-    end
-    
-	function euler = dcm_to_euler(obj, R)
-		euler = [atan2(R(3, 2), R(3, 3));
-			 asin(-R(3, 1));
-			 atan2(R(2, 1), R(1, 1))];
-    end
-    
-    function dcm = quaternion_to_dcm(obj, q0, q1, q2, q3)
-        %read: https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
-        q1q1 = q1 * q1;
-        q2q2 = q2 * q2;
-        q3q3 = q3 * q3;
-        q1q2 = q1 * q2;
-        q0q2 = q0 * q2;
-        q0q3 = q0 * q3;
-        q1q3 = q1 * q3;
-        q2q3 = q2 * q3;
-        q0q1 = q0 * q1;
-
-        dcm11 = 1.0 - 2.0 * (q2q2 + q3q3);
-        dcm12 = 2.0 * (q1q2 - q0q3);
-        dcm13 = 2.0 * (q0q2 + q1q3);
-        dcm21 = 2.0 * (q1q2 + q0q3);
-        dcm22 = 1.0 - 2.0 * (q1q1 + q3q3);
-        dcm23 = 2.0 * (q2q3 - q0q1);
-        dcm31 = 2.0 * (q1q3 - q0q2);
-        dcm32 = 2.0 * (q0q1 + q2q3);
-        dcm33 = 1.0 - 2.0 * (q1q1 + q2q2);
+    methods
+        function dcm = euler_to_dcm(obj, roll, pitch ,yaw)
+            %R = Rz(psi)Ry(theta)Rx(phi)
+            %read: https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+            cos_phi = cos(double(roll));
+            cos_theta = cos(pitch);
+            cos_psi = cos(yaw);
+            sin_phi = sin(roll);
+            sin_theta = sin(pitch);
+            sin_psi = sin(yaw);
+            dcm11 = cos_theta * cos_psi;
+            dcm12 = (-cos_phi * sin_psi) + (sin_phi * sin_theta * cos_psi);
+            dcm13 = (sin_phi * sin_psi) + (cos_phi * sin_theta * cos_psi);
+            dcm21 = cos_theta * sin_psi;
+            dcm22 = (cos_phi * cos_psi) + (sin_phi * sin_theta * sin_psi);
+            dcm23 = (-sin_phi * cos_psi) + (cos_phi * sin_theta * sin_psi);
+            dcm31 = -sin_theta;
+            dcm32 = sin_phi * cos_theta;
+            dcm33 = cos_phi * cos_theta;
+            dcm = [dcm11 dcm12 dcm13;
+                dcm21 dcm22 dcm23;
+                dcm31 dcm32 dcm33;];
+        end
         
-        dcm = [dcm11 dcm12 dcm13;
-		       dcm21 dcm22 dcm23;
-		       dcm31 dcm32 dcm33;];
+        function euler = dcm_to_euler(obj, R)
+            euler = [atan2(R(3, 2), R(3, 3));
+                asin(-R(3, 1));
+                atan2(R(2, 1), R(1, 1))];
+        end
+        
+        function dcm = quaternion_to_dcm(obj, q0, q1, q2, q3)
+            %read: https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+            q1q1 = q1 * q1;
+            q2q2 = q2 * q2;
+            q3q3 = q3 * q3;
+            q1q2 = q1 * q2;
+            q0q2 = q0 * q2;
+            q0q3 = q0 * q3;
+            q1q3 = q1 * q3;
+            q2q3 = q2 * q3;
+            q0q1 = q0 * q1;
+            
+            dcm11 = 1.0 - 2.0 * (q2q2 + q3q3);
+            dcm12 = 2.0 * (q1q2 - q0q3);
+            dcm13 = 2.0 * (q0q2 + q1q3);
+            dcm21 = 2.0 * (q1q2 + q0q3);
+            dcm22 = 1.0 - 2.0 * (q1q1 + q3q3);
+            dcm23 = 2.0 * (q2q3 - q0q1);
+            dcm31 = 2.0 * (q1q3 - q0q2);
+            dcm32 = 2.0 * (q0q1 + q2q3);
+            dcm33 = 1.0 - 2.0 * (q1q1 + q2q2);
+            
+            dcm = [dcm11 dcm12 dcm13;
+                dcm21 dcm22 dcm23;
+                dcm31 dcm32 dcm33;];
+        end
+        
+        function quat = dcm_to_quaternion(obj, R)
+            q1 = sqrt(0.25 * (1 + R(1, 1) - R(2, 2) - R(3, 3)));
+            div_4q1 = 1.0 / (4.0 * q1);
+            q2 = (R(2, 1) + R(1, 2)) * div_4q1;
+            q3 = (R(3, 1) + R(1, 3)) * div_4q1;
+            q0 = (R(2, 3) - R(3, 2)) * -div_4q1;
+            quat = [q0; q1; q2; q3];
+        end
+        
+        function vec = vee_map_3x3(obj, mat)
+            vec=[mat(3, 2);
+                 mat(1, 3);
+                 mat(2, 1)];
+        end
+        
+        function mat = hat_map_3x3(obj, vec)
+            mat=[0.0 -vec(3) +vec(2);
+                 +vec(3) 0.0 -vec(1);
+                 -vec(2) +vec(1) 0.0];
+        end
+        
+        function R_orthonormal = dcm_orthonormalize(obj, R)
+            %read "Direction Cosine Matrix IMU: Theory" for detailed explanation
+            x = [R(1, 1) R(1, 2) R(1, 3)];
+            y = [R(2, 1) R(2, 2) R(2, 3)];
+            error = dot(x, y);
+            x_orthogonal = x - (0.5 * error * y);
+            y_orthogonal = y - (0.5 * error * x);
+            z_orthogonal = cross(x_orthogonal, y_orthogonal);
+            x_normalized = 0.5 * (3 - dot(x_orthogonal, x_orthogonal)) * x_orthogonal;
+            y_normalized = 0.5 * (3 - dot(y_orthogonal, y_orthogonal)) * y_orthogonal;
+            z_normalized = 0.5 * (3 - dot(z_orthogonal, z_orthogonal)) * z_orthogonal;
+            R_orthonormal = [x_normalized; y_normalized; z_normalized];
+        end
+        
+        function angle_rad = get_prv_angle(obj, R)
+            angle_rad = acos(0.5 * (R(1, 1) + R(2, 2) + R(3, 3) - 1));
+        end
     end
-    
-    function quat = dcm_to_quaternion(obj, R)
-        q1 = sqrt(0.25 * (1 + R(1, 1) - R(2, 2) - R(3, 3)));
-        div_4q1 = 1.0 / (4.0 * q1);
-        q2 = (R(2, 1) + R(1, 2)) * div_4q1;
-        q3 = (R(3, 1) + R(1, 3)) * div_4q1;
-        q0 = (R(2, 3) - R(3, 2)) * -div_4q1;
-        quat = [q0; q1; q2; q3];
-    end
-
-	function vec = vee_map_3x3(obj, mat)
-		vec=[mat(3, 2);
-		     mat(1, 3);
-		     mat(2, 1)];
-	end
-
-	function mat = hat_map_3x3(obj, vec)
-		mat=[0.0 -vec(3) +vec(2);
-		     +vec(3) 0.0 -vec(1);
-		     -vec(2) +vec(1) 0.0];
-	end
-
-	function R_orthonormal = dcm_orthonormalize(obj, R)
-		%read "Direction Cosine Matrix IMU: Theory" for detailed explanation
-		x = [R(1, 1) R(1, 2) R(1, 3)];
-		y = [R(2, 1) R(2, 2) R(2, 3)];
-		error = dot(x, y);
-		x_orthogonal = x - (0.5 * error * y);
-		y_orthogonal = y - (0.5 * error * x);
-		z_orthogonal = cross(x_orthogonal, y_orthogonal);
-		x_normalized = 0.5 * (3 - dot(x_orthogonal, x_orthogonal)) * x_orthogonal;
-		y_normalized = 0.5 * (3 - dot(y_orthogonal, y_orthogonal)) * y_orthogonal;
-		z_normalized = 0.5 * (3 - dot(z_orthogonal, z_orthogonal)) * z_orthogonal;
-		R_orthonormal = [x_normalized; y_normalized; z_normalized];
-	end
-
-	function angle_rad = get_prv_angle(obj, R)
-		angle_rad = acos(0.5 * (R(1, 1) + R(2, 2) + R(3, 3) - 1));
-	end
-	end
 end
